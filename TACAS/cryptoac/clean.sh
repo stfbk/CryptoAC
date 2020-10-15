9cc2baf5d25895642207a7b3b4acfa46e2c2169e
StefanoBerlato <sb.berlatostefano@gmail.com>
2020-10-15 17:03:56 +0200

    merge

--- TACAS/cryptoac/clean.sh
+++
@@ -1,9 +0,0 @@
-docker rm cryptoac_rm_1
-docker rm cryptoac_ms_1
-docker rm cryptoac_ds_1
-docker rm cryptoac_proxy_1
-
-docker rmi cryptoac_rm
-docker rmi cryptoac_ms
-docker rmi cryptoac_ds
-docker rmi cryptoac_proxy
